c8b79d3f4392b52c87c77d3d815fc8e38c959305
hareeshreddy8 <hareeshreddychinnakondu48@outlook.com>
2025-11-05 19:24:39 +0530

    solution uploaded

---
+++ Arrays _and_hashing/longestconsecutivies.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/longest-consecutive-sequence/
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums :
+            return 0 
+        
+        nums_set = set(nums)
+        longest = 0
+        for num in nums_set :
+            if num - 1  not in nums_set :
+                current = num 
+                streak = 1
+                while current + 1 in nums_set :
+                    current += 1
+                    streak += 1
+            
+                longest = max(longest,streak)
+        return longest
